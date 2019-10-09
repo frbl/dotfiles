98e2077997b7dcc3f268d741a4e7477570368487
Frank Blaauw <frank.blaauw@gmail.com>
2019-10-09 15:10:46 +0200

    Updating local changes

--- sync.sh
+++ sync.sh
@@ -11,5 +11,7 @@ git commit -am 'Updating local changes'
 git push
 
 cd $CACHE_LOCATION
+mkdir -p CACHE_BACKUP
+git status --short | awk {'print $2'} | xargs -I{} mv {} CACHE_BACKUP
 git co -- .
 git pull
