cd1857fa63f7f48bf66919899844efbff702c53e
Frank Blaauw <frank.blaauw@gmail.com>
2020-01-11 22:38:48 +0100

    Updating local changes

--- sh/profile_files.symlink/alias.sh
+++ sh/profile_files.symlink/alias.sh
@@ -281,6 +281,15 @@ duald() {
   xrandr --output HDMI-1 --left-of LVDS-1
 }
 
+dualdrighthnk() {
+  xrandr --output VGA-1 --off
+  xrandr --output HDMI-1 --auto
+  xrandr --output LVDS-1 --auto
+  xrandr --output eDP-1 --auto
+  xrandr --output DP-1 --right-of LVDS-1
+  xrandr --output DP-1 --right-of eDP-1
+}
+
 dualdright() {
   xrandr --output VGA-1 --off
   xrandr --output HDMI-1 --auto
@@ -307,6 +316,7 @@ dualdvga() {
 singled() {
   xrandr --output HDMI-1 --off
   xrandr --output VGA-1 --off
+  xrandr --output DP-1 --off
   xrandr --output LVDS-1 --auto
   xrandr --output eDP-1 --auto
 }
