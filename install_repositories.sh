618a352232f381a5f0ff607adce1f50175e2329b
Frank Blaauw <frank.blaauw@gmail.com>
2020-03-07 21:01:48 +0100

    Updating local changes

--- install_repositories.sh
+++ install_repositories.sh
@@ -116,13 +116,13 @@ gitlab_clone "researchable/sport-data-valley/MVP" "svc-data-integrations"
 gitlab_clone "researchable/sport-data-valley/MVP" "svc-questionnaires"
 gitlab_clone "researchable/sport-data-valley/MVP" "jupyterhub"
 
-#gitlab_clone "sport-data-valley" "poc-data"
+gitlab_clone "sport-data-valley" "poc-data"
 
 # i-shared
 #gitlab_clone "researchable/samen-sturen" "svc-visualization-r"
-#gitlab_clone "researchable/samen-sturen" "svc-visualization-js"
-#gitlab_clone "researchable/samen-sturen" "svc-prediction"
 gitlab_clone "researchable/samen-sturen" "svc-pdfgenerator"
+gitlab_clone "researchable/samen-sturen" "svc-visualization-js"
+gitlab_clone "researchable/samen-sturen" "svc-prediction"
 gitlab_clone "researchable/samen-sturen" "composer"
 gitlab_clone "researchable/samen-sturen" "infrastructure"
 
