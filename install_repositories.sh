05bade88ff3d0dcc142fef6c7de5ff5ac2800eca
Frank Blaauw <frank.blaauw@gmail.com>
2020-07-06 14:50:11 +0200

    Added more repos

--- install_repositories.sh
+++ install_repositories.sh
@@ -137,6 +137,7 @@ roqua_clone "ishared" "svc-visualization-r" "researchable/samen-sturen"
 
 roqua_clone "roqua" "roqua" "roqua"
 roqua_clone "roqua" "compose" "roqua"
+roqua_clone "roqua" "core" "roqua"
 roqua_clone "roqua" "questionnaires" "roqua" 
 
 #gitlab_clone "roqua" "autovar"
