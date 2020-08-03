4b0d3543281576623f4949658b40358b29cc38d0
Frank Blaauw <frank.blaauw@gmail.com>
2020-08-03 14:55:51 +0200

    Added fitbit model repo

--- install_repositories.sh
+++ install_repositories.sh
@@ -118,13 +118,14 @@ gitlab_clone "researchable/sport-data-valley/MVP" "svc-data-integrations"
 gitlab_clone "researchable/sport-data-valley/MVP" "svc-questionnaires"
 gitlab_clone "researchable/sport-data-valley/MVP" "jupyterhub"
 gitlab_clone "researchable/sport-data-valley/MVP" "svc-backups"
+gitlab_clone "researchable/sport-data-valley/MVP" "svc-data-mining-analysis-fitbit"
 
 #gitlab_clone "sport-data-valley" "poc-data"
 
 # i-shared
 #gitlab_clone "researchable/samen-sturen" "svc-visualization-r"
 gitlab_clone "researchable/samen-sturen" "svc-pdfgenerator"
-gitlab_clone "researchable/samen-sturen" "svc-prediction"
+#gitlab_clone "researchable/samen-sturen" "svc-prediction"
 gitlab_clone "researchable/samen-sturen" "composer"
 gitlab_clone "researchable/samen-sturen" "infrastructure"
 
