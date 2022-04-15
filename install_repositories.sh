c943e9ed0e0aa50156438ff9d081004a9d532001
Frank Blaauw <f.j.blaauw@researchable.nl>
2022-04-15 22:15:13 +0200

    chore: updated repos

--- install_repositories.sh
+++ install_repositories.sh
@@ -183,6 +183,8 @@ gitlab_clone "researchable/general/security" "postgres_backup_checker"
 gitlab_clone "researchable/general/security" "mongodb_backup_checker"
 gitlab_clone "researchable/general/security" "general-fix-scripts"
 
+gitlab_clone "researchable/projects/certibot" "docs"
+
 gitlab_clone "researchable/general/management" "freedcamp-hour-counter"
 gitlab_clone "researchable/general/management" "freedcamp-hour-importer"
 gitlab_clone "researchable/general/management" "bookkeeper"
